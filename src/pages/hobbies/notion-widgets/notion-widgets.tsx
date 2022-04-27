a771bbefb214774695ac9cbb8ec4a8c01560f54c
hfsaito <hiroshi.fs94@gmail.com>
2022-04-27 19:20:38 +0200

    Countdown widget with query param

--- src/pages/hobbies/notion-widgets/notion-widgets.tsx
+++ src/pages/hobbies/notion-widgets/notion-widgets.tsx
@@ -6,10 +6,7 @@ export const NotionWidgetsPage: React.FC = () => (
   <>
     <H1>Notion Widgets</H1>
     <div>
-      <ExtLink href="/notion-widgets/countdown-1.html">countdown-1</ExtLink>
-    </div>
-    <div>
-      <ExtLink href="/notion-widgets/countdown-2.html">countdown-2</ExtLink>
+      <ExtLink href="/notion-widgets/countdown.html?date=2022-05-19T20:00:00.000-03:00">countdown</ExtLink>
     </div>
   </>
 );
