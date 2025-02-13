1b74856fc628ee554e2fc9c4e9c70741e14f49c8
hfsaito <hiroshi.fs94@gmail.com>
2025-02-13 20:21:40 -0300

    fix: existing code after dependencies update

--- src/mod-tool/apis/twitch/requester.ts
+++ src/mod-tool/apis/twitch/requester.ts
@@ -1,20 +1,18 @@
-import axios, { AxiosRequestConfig } from 'axios';
+import axios, { InternalAxiosRequestConfig } from 'axios';
 
 import { TWITCH_API_CLIENT_ID } from './constants';
 import { AuthManager } from './auth';
 
 export const requester = axios.create();
-requester.interceptors.request.use(async (config: AxiosRequestConfig) => {
+requester.interceptors.request.use(async (config: InternalAxiosRequestConfig) => {
   const token = await AuthManager.getToken();
   if (!token) {
     throw 'Missing token'
   }
-  config.headers = {
-    ...(config.headers ?? {}),
-    Authorization: `Bearer ${token}`,
-    'Client-Id': TWITCH_API_CLIENT_ID,
-    'Content-Type': 'application/json'
-  };
+
+  config.headers.set('Authorization', `Bearer ${token}`);
+  config.headers.set('Client-Id', TWITCH_API_CLIENT_ID);
+  config.headers.set('Content-Type', 'application/json');
   return config;
 }, () => null);
 
--- src/mod-tool/core/setup.tsx
+++ src/mod-tool/core/setup.tsx
@@ -5,6 +5,5 @@ import * as ReactDOM from 'react-dom';
 import { Root } from './root';
 
 export const setup = () => {
-  axios.defaults.adapter = require('axios/lib/adapters/http'); // always use Node.js adapter
   ReactDOM.render(<Root />, document.querySelector('#root'));
 }
